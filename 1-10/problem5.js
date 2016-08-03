6fd536f2489bba08334e4229ce60d85fbd5d8c85
Krueger, Brandon <Brandon.Krueger@wwt.com>
2016-08-03 08:33:00 -0500

    Problem 5 solution

--- 1-10/problem5.js
+++ 1-10/problem5.js
@@ -5,3 +5,35 @@
  https://projecteuler.net/problem=5
 */
 
+const upperBound = 20
+const answer = findSmallestDivisable(upperBound)
+console.log(answer)
+
+function findSmallestDivisable(upperBound) {
+  let result = 0
+
+  let multiplier = 1
+  while (!result) {
+
+    const solution = isDivisable(upperBound, multiplier)
+    if (solution) {
+      result = solution
+    }
+
+    multiplier++
+  }
+
+  return result
+}
+
+function isDivisable(upperBound, multiplier) {
+  const possibleResult = upperBound * multiplier
+
+  for (let i = upperBound - 1; i > 0; i--) {
+    if (possibleResult % i != 0) {
+      return false
+    }
+  }
+
+  return possibleResult
+}
